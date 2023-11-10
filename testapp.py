f31ebd23176f62b3b3bf608b6b045bfa225827e3
Rainbow Li <rainbowli1029@hotmail.com>
2023-11-10 16:07:14 -0500

    update

--- testapp.py
+++ testapp.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 
 # Initialize the OpenAI client with your API key
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 # Upload a file with an "assistants" purpose
 file = client.files.create(
